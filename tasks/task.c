08ffd54ad05b47952708ed938c332e26ffb14898
Morgan-Harvey <146832750+Morgan-Harvey@users.noreply.github.com>
2024-02-23 12:20:21 +0000

    Task_done

--- tasks/task.c
+++ tasks/task.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    int num1, num2;
+int main(int argc, char *argv[]) {
+    // check command line for arguments
+    if(argc != 3){
 
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    printf("Usage: %s <First Number> <Second Number>\n", argv[0]);
+    return 0;
+    }
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    // stores arguments 
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
+
 
     // Calculate and print the sum
     int sum = num1 + num2;
